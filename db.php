90e999d62251455eda5595eaab3c8690ac931944
Tsuribori <tsuribori@tutanota.com>
2017-11-03 11:44:32 +0200

    Added creation dates to pastes

--- db.php
+++ db.php
@@ -9,27 +9,29 @@ function create_database() {
     $db = "";
     if ($db  = new SQLite3($database_name . ".db")) {
         $db->exec('CREATE TABLE users (name TEXT PRIMARY KEY, password TEXT NOT NULL)');
-        $db->exec('CREATE TABLE text (id INTEGER PRIMARY KEY, paste TEXT NOT NULL, title TEXT NOT NULL, name TEXT NOT NULL, code INTEGER NOT NULL)'); #Create table and columns
+        $db->exec('CREATE TABLE text (id INTEGER PRIMARY KEY, paste TEXT NOT NULL, title TEXT NOT NULL, name TEXT NOT NULL, code INTEGER NOT NULL, date TEXT NOT NULL)'); #Create table and columns
 	echo "success";
         $create_values = $db->prepare("INSERT INTO users (name, password) VALUES ('FIRSTUSER', 'umadelicia')");
         $create_values->execute();
-        $create_values2 = $db->prepare("INSERT INTO text (id, paste, title, name, code) VALUES (1, 'FIRST', 'FIRST', 'FIRST', 0)"); #Create the first row so that insert_paste() works
+        $create_values2 = $db->prepare("INSERT INTO text (id, paste, title, name, code, date) VALUES (1, 'FIRST', 'FIRST', 'FIRST', 0, 'Monday morning')"); #Create the first row so that insert_paste() works
         $create_values2->execute();
+        $db->close();
         
     }
    else {
         die("Unable to create database!");
     }
 }
-$database_name = $database_name . ".db";
-$db = new SQLite3($database_name);
+
 #Insert the paste
 function insert_paste() {
    global $database_name;
    global $db;
-   if (!file_exists($database_name . ".db")) {   #Create table if it doesn't exist
-   create_database();
+   if (!file_exists($db)) {   #Create table if it doesn't exist
+       create_database();
    }
+   $database_name = $database_name . ".db";
+   $db = new SQLite3($database_name);
    $statement = $db->prepare("SELECT id FROM text WHERE id = (SELECT MAX(ID) FROM text)"); #Get the last id number
    if ($last_id = $statement->execute()) {
    }
@@ -47,18 +49,21 @@ function insert_paste() {
    else {
        $code = 0;
    }
-   $statement2 = $db->prepare("INSERT INTO text (id, paste, title, name, code) VALUES (?, ?, ?, ?, ?)"); #Prepare insert statement
+   $statement2 = $db->prepare("INSERT INTO text (id, paste, title, name, code, date) VALUES (?, ?, ?, ?, ?, ?)"); #Prepare insert statement
    if ($_SESSION["confirmation"]) {
       $name = $_SESSION["confirmation"];
    }
    else {
       $name = "Anonymous";
    }
+ 
+   $date = date("m.d.Y, g:i a"); 
    $statement2->bindValue(1, $id, PDO::PARAM_INT);  #Bind parameters to statement
    $statement2->bindValue(2, $paste);
    $statement2->bindValue(3, $title);
    $statement2->bindValue(4, $name);
    $statement2->bindValue(5, $code); 
+   $statement2->bindValue(6, $date);
    if ($result = $statement2->execute()) {    #Execute the statement
       $id = $id;
       $db->close();
